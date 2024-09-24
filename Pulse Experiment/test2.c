cd103a96a65c63655aca78436f98ccc0e7a95d56
llukeandersonn <152121128+llukeandersonn@users.noreply.github.com>
2024-09-24 13:48:00 -0600

    Add files via upload

---
+++ Pulse Experiment/test2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "pigpiod_if2.h"
+
+
+
+int main(int argc, char *argv[]){
+    //gpioInitialise();
+    int start = pigpio_start(NULL, NULL);
+    set_mode(start, 23, PI_OUTPUT);
+    gpio_write(start, 23, 1);
+    time_sleep(5);
+    gpio_write(start, 23, 0);
+    time_sleep(5);
+    set_mode(start, 21, PI_OUTPUT);
+    gpio_write(start, 21, 1);
+    time_sleep(5);
+    gpio_write(start, 21, 0);
+}
