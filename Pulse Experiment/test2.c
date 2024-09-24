9c3149b207d70a0ee2d71adac8e60612342bf82b
llukeandersonn <152121128+llukeandersonn@users.noreply.github.com>
2024-09-24 13:54:13 -0600

    Update test2.c

--- Pulse Experiment/test2.c
+++ Pulse Experiment/test2.c
@@ -22,4 +22,14 @@ int main(int argc, char *argv[]){
     gpio_write(start, 21, 1);
     time_sleep(5);
     gpio_write(start, 21, 0);
+
+    time_sleep(0.5);
+    //test looping in 5 ten-thousandths of a second
+    while (1) {
+    gpio_write(start, 23, 1);
+    time_sleep(0.0005);
+    gpio_write(start, 23, 0);
+    time_sleep(0.0005)
+    }
+    
 }
